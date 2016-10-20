63f07ba0f59538fb157428586d5a8a8f516c068a
Diego Martinez <zerodie.7@hotmail.com>
2016-10-20 02:42:58 -0500

    Proyecto N°1

---
+++ Proyecto1_1/src/GLApplication.cpp
@@ -0,0 +1,242 @@
+/*
+ * GLApplication.cpp
+ *
+ *  Created on: 07/08/2016
+ *      Author: rey
+ */
+
+#include "Headers/GLApplication.h"
+
+GLuint VAO, VBO, EBO;
+Sphere sp(1.5, 50, 50, MODEL_MODE::VERTEX_LIGHT_TEXTURE);
+Sphere sp2(1.5, 50, 50, MODEL_MODE::VERTEX_LIGHT_TEXTURE);
+Shader lightingShader;
+Shader lampShader;
+Shader shader;
+Shader sistemascoordenados;
+Texture textureDifuse(GL_TEXTURE_2D, "Textures/SUN.jpg");
+Texture textureSpecular(GL_TEXTURE_2D, "Textures/SUN_SPECULAR.JPG");
+
+struct VertexTexture {
+	glm::vec3 m_Pos;
+	glm::vec2 m_TexCoord;
+};
+
+GLApplication::GLApplication() :
+		windowManager(nullptr), camera(nullptr) {
+}
+
+GLApplication::~GLApplication() {
+	destroy();
+}
+
+void GLApplication::GLMain() {
+	initialize();
+	applicationLoop();
+}
+
+void GLApplication::initialize() {
+	if (!windowManager || !windowManager->initialize(800, 700, "Window GLFW", false)) {
+		this->destroy();
+		exit(-1);
+	}
+
+	glViewport(0, 0, WindowManager::screenWidth, WindowManager::screenHeight);
+	glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_DEPTH_TEST);
+
+	sp.init();
+	sp.load();
+
+	sp2.init();
+	sp2.load();
+
+	textureDifuse.load();
+	textureSpecular.load();
+
+	sistemascoordenados.initialize("Shaders/sistemascoordenados.vs", "Shaders/sistemascoordenados.fs");
+	shader.initialize("Shaders/shader.vs", "Shaders/shader.fs");
+	lightingShader.initialize("Shaders/lightingSpecularMap.vs","Shaders/lightingSpecularMap.fs");
+	lampShader.initialize("Shaders/lampShader.vs", "Shaders/lampShader.fs");
+
+
+}
+
+void GLApplication::applicationLoop() {
+	bool processInput = true;
+	glm::vec3 lightPos( -2.0f, -2.0f, -2.0f);
+	Model objModel("objects/green_arrow/Green_Arrow_A.obj");
+	Model objModel1("objects/flash/Flash.obj");
+	Model objModel2("objects/death/Deathstroke.obj");
+
+	double lastTime = TimeManager::Instance().GetTime();
+	while (processInput) {
+		processInput = windowManager->processInput(true);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+
+		//Rotar esfera de luz
+		GLfloat timeValue = TimeManager::Instance().GetTime() - lastTime;
+		GLfloat valX = sin((GLfloat)timeValue * 1.0f) * 3.0f;
+		GLfloat valY = sin((GLfloat)timeValue * 3.1416f/2) * 6.0f;
+		GLfloat valZ = cos((GLfloat)timeValue * 1.0f) * 3.0f;
+		lightPos.x = valX;
+		lightPos.y = valY;
+		lightPos.z = valZ;
+
+
+		//dibujar la esfera
+		lightingShader.turnOn();
+		GLint viewPosLoc = lightingShader.getUniformLocation("viewPos");
+		glUniform3f(viewPosLoc, lightPos.x, lightPos.y, lightPos.z);
+
+		// Transformation matrices
+		glm::vec3 cameraPos = windowManager->inputManager.getCameraPos();
+		glm::vec3 cameraLookAt = windowManager->inputManager.getCameraPos() + windowManager->inputManager.getCameraDirection();
+		glm::mat4 view = glm::lookAt(cameraPos, cameraLookAt, glm::vec3(0, 0.5, 0.0));
+		//glm::mat4 view = camera->GetViewMatrix();
+		glm::mat4 projection;
+		projection = glm::perspective(45.0f, (GLfloat)WindowManager::screenWidth / (GLfloat)WindowManager::screenHeight, 0.1f, 100.0f);
+
+
+		// Get the uniform locations
+		GLint modelLoc = lightingShader.getUniformLocation("model");
+		GLint viewLoc = lightingShader.getUniformLocation("view");
+		GLint projLoc = lightingShader.getUniformLocation("projection");
+		
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Set material properties
+		GLint matDiffuseLoc = lightingShader.getUniformLocation("material.diffuse");
+		GLint matSpecularLoc = lightingShader.getUniformLocation("material.specular");
+		GLint matShineLoc = lightingShader.getUniformLocation("material.shininess");
+		glUniform1i(matDiffuseLoc, 0);
+		glUniform1i(matSpecularLoc, 1);
+		glUniform1f(matShineLoc, 32.0f);
+
+		// Set lights properties
+		GLint lightAmbientLoc = lightingShader.getUniformLocation("light.ambient");
+		GLint lightDiffuseLoc = lightingShader.getUniformLocation("light.diffuse");
+		GLint lightSpecularLoc = lightingShader.getUniformLocation("light.specular");
+		GLint lightPosLoc = lightingShader.getUniformLocation("light.position");
+		glUniform3f(lightAmbientLoc, 0.2f, 0.2f, 0.2f);
+		glUniform3f(lightDiffuseLoc, 0.9f, 0.9f, 0.9f);
+		glUniform3f(lightSpecularLoc, 1.0f, 1.0f, 1.0f);
+		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+		//Fin codigo luz
+
+		// Draw a sphere sol
+		glm::mat4 model0;
+		model0 = glm::translate(model0, lightPos);
+		model0 = glm::scale(model0, glm::vec3(0.6, 0.6, 0.6));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model0));
+		textureDifuse.bind(GL_TEXTURE0);
+		textureSpecular.bind(GL_TEXTURE1);
+		sp2.render();
+		lightingShader.turnOff();
+		//acabar con la esfera sol
+
+
+		//Inicio codigo de modelos
+		shader.turnOn();
+
+		// Get the uniform locations
+		modelLoc = shader.getUniformLocation("model");
+		viewLoc = shader.getUniformLocation("view");
+		projLoc = shader.getUniformLocation("projection");
+
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Set material properties
+		// matDiffuseLoc = lightingShader.getUniformLocation("material.diffuse");
+		// matSpecularLoc = lightingShader.getUniformLocation("material.specular");
+		matShineLoc = lightingShader.getUniformLocation("material.shininess");
+		// glUniform1i(matDiffuseLoc, 0);
+		// glUniform1i(matSpecularLoc, 1);
+		glUniform1f(matShineLoc, 32.0f);
+
+		// Set lights properties
+		lightAmbientLoc = lightingShader.getUniformLocation("light.ambient");
+		lightDiffuseLoc = lightingShader.getUniformLocation("light.diffuse");
+		lightSpecularLoc = lightingShader.getUniformLocation("light.specular");
+		lightPosLoc = lightingShader.getUniformLocation("light.position");
+		glUniform3f(lightAmbientLoc, 0.2f, 0.2f, 0.2f);
+		glUniform3f(lightDiffuseLoc, 0.9f, 0.9f, 0.9f);
+		glUniform3f(lightSpecularLoc, 1.0f, 1.0f, 1.0f);
+		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+
+
+		// Draw the loaded model
+		glm::mat4 model;
+		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		objModel.render(&shader);
+
+		// Draw the loaded model1
+		glm::mat4 model1;
+		model1 = glm::translate(model1, glm::vec3(0.75f, -1.75f, 0.0f));
+		model1 = glm::scale(model1, glm::vec3(0.2f, 0.2f, 0.2f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model1));
+		objModel1.render(&shader);
+
+		// Draw the loaded model2
+		glm::mat4 model2;
+		model2 = glm::translate(model2, glm::vec3(-0.75f, -1.75f, 0.0f));
+		model2 = glm::scale(model2, glm::vec3(0.2f, 0.2f, 0.2f));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
+		objModel2.render(&shader);
+
+		shader.turnOff();
+	//Fin dibujo modelos
+
+	//Dibujo esfera iluminacion
+		lampShader.turnOn();
+
+		// Create transformations
+		modelLoc = lampShader.getUniformLocation("model");
+		viewLoc = lampShader.getUniformLocation("view");
+		projLoc = lampShader.getUniformLocation("projection");
+
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		model0 = glm::scale(model0, glm::vec3(0.1, 0.1, 0.1));
+		model0 = glm::translate(model0, lightPos);
+		//model = glm::scale(model, glm::vec3(0.4, 0.4, 0.4));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model0));
+		sp.render();
+		lampShader.turnOff();
+	
+
+		windowManager->swapTheBuffers();
+	}
+}
+
+void GLApplication::destroy() {
+	if (windowManager) {
+		delete windowManager;
+		windowManager = nullptr;
+	}
+
+	glDisableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glDeleteBuffers(1, &EBO);
+
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &VAO);
+
+}
---
+++ Proyecto1_1/src/InputManager.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+
+#include "Headers/InputManager.h"
+#include "Headers/TimeManager.h"
+
+InputCodes InputManager::toApplicationKey(int key) {
+	switch (key) {
+	case 256:
+		return InputCodes::kEscape;
+	case 87:
+		return InputCodes::W;
+	case 83:
+		return InputCodes::S;
+	case 65:
+		return InputCodes::A;
+	case 68:
+		return InputCodes::D;
+	case 265:
+		return InputCodes::Up;
+	case 264:
+		return InputCodes::Down;
+	case 263:
+		return InputCodes::Left;
+	case 262:
+		return InputCodes::Right;
+	}
+}
+
+State InputManager::toApplicationState(int state) {
+	switch (state) {
+	case 0:
+		return State::RELESED;
+	case 1:
+		return State::PRESSED;
+	}
+}
+
+MouseButtonIndex InputManager::toMouseButtonIndex(int button) {
+	switch (button) {
+	case 0:
+		return MouseButtonIndex::LEFT;
+	case 1:
+		return MouseButtonIndex::RIGHT;
+	case 2:
+		return MouseButtonIndex::MIDDLE;
+	}
+}
+
+void InputManager::keyPressed(InputCodes code, float deltaTime, State state) {
+	keyState[code] = state;
+}
+
+void InputManager::mouseMoved(float mouseX, float mouseY) {
+	if (mouseButtomState[MouseButtonIndex::LEFT]
+		|| mouseButtomState[MouseButtonIndex::RIGHT]) {
+		deltax = mouseX - lastMousePos.x;
+		deltay = mouseY - lastMousePos.y;
+	}
+	else {
+		deltax = 0;
+		deltay = 0;
+	}
+	lastMousePos.x = mouseX;
+	lastMousePos.y = mouseY;
+}
+
+/* FPS
+void InputManager::mouseMoved(float mouseX, float mouseY) {
+	if (mouseButtomState[MouseButtonIndex::LEFT]) {
+		float xoffset = mouseX - lastMousePos.x;
+		float yoffset = lastMousePos.y - mouseY;
+		camera->ProcessMouseMovement(xoffset, yoffset);
+	}
+	lastMousePos.x = mouseX;
+	lastMousePos.y = mouseY;
+}
+*/
+
+void InputManager::mouseClicked(MouseButtonIndex code, float mouseX,
+	float mouseY, State state) {
+	mouseButtomState[code] = state;
+	switch (code) {
+	case RIGHT:
+		lastMousePos.x = mouseX;
+		lastMousePos.y = mouseY;
+		break;
+	case LEFT:
+		lastMousePos.x = mouseX;
+		lastMousePos.y = mouseY;
+		break;
+	case MIDDLE:
+		break;
+	}
+}
+/*FPS
+void InputManager::mouseClicked(MouseButtonIndex code, float mouseX,
+		float mouseY, State state) {
+	switch (code) {
+	case RIGHT:
+		std::cout << "lastMousePos.x:" << lastMousePos.x << std::endl;
+		break;
+	case LEFT:
+		std::cout << "lastMousePos.y:" << lastMousePos.y << std::endl;
+		lastMousePos.x = mouseX;
+		lastMousePos.y = mouseY;
+		mouseButtomState[code] = state;
+		break;
+	case MIDDLE:
+		std::cout << "lastMousePos.x:" << lastMousePos.x << std::endl;
+		std::cout << "lastMousePos.y:" << lastMousePos.y << std::endl;
+		break;
+	}
+}
+*/
+
+
+void InputManager::mouseScroll(float yoffset) {
+	scrollYoffset = yoffset;
+}
+/*FPS
+void InputManager::mouseScroll(float yoffset) {
+}
+*/
+
+
+void InputManager::do_movement(float deltaTime) {
+	// Camera controls
+	float cameraSpeed = 50.0f * deltaTime;
+	glm::vec3 camera_look_at = glm::vec3(-0.75f, -1.0f, 0.0f); //Camara seleccionada
+	float dx = 0, dy = 0;
+
+	// Calculate zoom
+	float zoomLevel = scrollYoffset * cameraSpeed;
+	distanceFromPlayer -= zoomLevel;
+
+	// Calculate pitch
+	if (mouseButtomState[MouseButtonIndex::RIGHT])
+		dy = deltay;
+	pitch -= dy * cameraSpeed;
+	std::cout << "pitch:" << pitch << std::endl;
+
+	// Calculate Angle Arround
+	if (mouseButtomState[MouseButtonIndex::LEFT])
+		dx = deltax;
+	angleAroundPlayer -= dx * cameraSpeed;
+	std::cout << "angleAroundPlayer:" << angleAroundPlayer << std::endl;
+
+	//Calculate Horizontal distance
+	float horizontalDistance = distanceFromPlayer
+		* glm::cos(glm::radians(pitch));
+	//Calculate Vertical distance
+	float verticalDistance = distanceFromPlayer * glm::sin(glm::radians(pitch));
+
+	//Calculate camera position
+	float theta = 0 + angleAroundPlayer;
+	float offsetx = horizontalDistance * glm::sin(glm::radians(theta));
+	float offsetz = horizontalDistance * glm::cos(glm::radians(theta));
+	cameraPos.x = camera_look_at.x - offsetx;
+	cameraPos.z = camera_look_at.z - offsetz;
+	cameraPos.y = camera_look_at.y + verticalDistance;
+
+	yaw = 0 - (180 + angleAroundPlayer);
+
+	if (distanceFromPlayer < 0)
+		cameraDirection = glm::normalize(cameraPos - camera_look_at);
+	else
+		cameraDirection = glm::normalize(camera_look_at - cameraPos);
+
+	scrollYoffset = 0;
+	deltax = 0;
+	deltay = 0;
+
+}
+
+/*FPS
+void InputManager::do_movement(float deltaTime) {
+	if (keyState[InputCodes::W])
+		camera->ProcessKeyboard(C_FORWARD, deltaTime);
+	if (keyState[InputCodes::S])
+		camera->ProcessKeyboard(C_BACKWARD, deltaTime);
+	if (keyState[InputCodes::A])
+		camera->ProcessKeyboard(C_LEFT, deltaTime);
+	if (keyState[InputCodes::D])
+		camera->ProcessKeyboard(C_RIGHT, deltaTime);
+}
+*/
